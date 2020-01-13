fdfd33806c2d3465da9fd53ebff67c79b04aa86e
Mark Willems <mark.willems@isatis-health.nl>
2020-01-13 21:34:49 +0100

    autoupdater progress

--- elrond-updater/updater.sh
+++ elrond-updater/updater.sh
@@ -1,22 +1,41 @@
+FROM ubuntu:18.04
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update -y && apt-get upgrade -y && apt install -y curl  apt-utils wget git docker.io
+RUN cd /lib && wget https://github.com/wasmerio/wasmer/releases/download/0.12.0/libwasmer_runtime_c_api.so
+ADD updater.sh /opt/updater.sh
+RUN chmod +x /opt/updater.sh
+CMD ["/opt/updater.sh"]
+root@vultr:/opt/elrond-docker# cat elrond-updater/updater.sh
 #!/bin/bash
 
 set -e
-echo "Run autoupdater!"
+echo "$(date +%x_%H:%M:%S) start the update provess"
 
 function while_check {
     while true; do
-            echo "test"
-            docker ps
-for dir in /volumes/*/     # list directories in the form "/tmp/dirname/"
-do
-    dir=${dir%*/}      # remove the trailing "/"
-    echo ${dir##*/}    # print everything after the final "/"
-done
+        echo "$(date +%x_%H:%M:%S) starting an update round"
 
-     #docker restart  $(docker ps -aq --filter name=elrond-*)
-     ls /volumes/nodename/node  -all
-     
-     sleep 10s <&0 & wait
+        for dir in /volumes/*/ ; do
+            echo "[$(date +%x_%H:%M:%S)] check to see  if container $(basename $dir) is running"
+            if [ ! "$(docker ps -q -f name=$(basename dir))" ]; then
+                echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) found"
+                if [  -f /volumes/$(basename $dir)/node ]; then
+                    CURRENT="$(/volumes/$(basename $dir)/node -v)"
+                    LATEST=$(curl --silent "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+                    if [[ $CURRENT =~ $LATEST ]]; then
+                        echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running up to date with version $CURRENT"
+                    else
+                        echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running $CURRENT and is not the latest $LATEST, execute restart"
+                        docker restart $(basename $dir)
+                    fi
+                else
+                         echo "$(date +%x_%H:%M:%S) /volumes/$(basename $dir)/node not found"
+                fi
+            fi
+        done
+        echo "[$(date +%x_%H:%M:%S)] update round done"
+        sleep 480s <&0 & wait
     done
 }
 
