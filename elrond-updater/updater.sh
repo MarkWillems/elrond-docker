d0e292c26c796cba673782871391fccd55f8d8a2
Mark Willems <mark.willems@isatis-health.nl>
2020-01-14 19:52:34 +0100

    stop the looping  and github token

--- elrond-updater/updater.sh
+++ elrond-updater/updater.sh
@@ -9,24 +9,26 @@ function while_check {
 
         for dir in /volumes/*/ ; do
             echo "[$(date +%x_%H:%M:%S)] check to see  if container $(basename $dir) is running"
-            if [ ! "$(docker ps -q -f name=$(basename dir))" ]; then
-                echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) found"
-                if [  -f /volumes/$(basename $dir)/node ]; then
-                    CURRENT="$(/volumes/$(basename $dir)/node -v)"
-                    if [ -z "$GITHUBTOKEN" ]; then 
-                        LATEST="tags/$(curl --silent "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
-                    else      
-                        LATEST="tags/$(curl --silent -H "Authorization: token $GITHUBTOKEN" "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
-                    fi
-                    if [[ $CURRENT =~ $LATEST ]]; then
-                        echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running up to date with version $CURRENT"
-                    else
-                        echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running $CURRENT and is not the latest $LATEST, execute restart"
-                        docker restart $(basename $dir)
-                    fi
-                else
-                         echo "$(date +%x_%H:%M:%S) /volumes/$(basename $dir)/node not found"
-                fi
+            if [  "$(docker ps -q -f name=$(basename dir))" ]; then
+                continue
+            fi
+            echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) found"
+
+            if [ ! -f /volumes/$(basename $dir)/node ]; then
+                echo "$(date +%x_%H:%M:%S) /volumes/$(basename $dir)/node not found"
+            fi
+
+            CURRENT="$(/volumes/$(basename $dir)/node -v)"
+            if [ -z "$GITHUBTOKEN" ]; then 
+                LATEST="tags/$(curl --silent "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+            else      
+                LATEST="tags/$(curl --silent -H "Authorization: token $GITHUBTOKEN" "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+            fi
+            if [[ $CURRENT =~ $LATEST ]]; then
+                echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running up to date with version $CURRENT"
+            else
+                echo "[$(date +%x_%H:%M:%S)] container $(basename $dir) is running $CURRENT and is not the latest $LATEST, execute restart"
+                docker restart $(basename $dir)
             fi
         done
         echo "[$(date +%x_%H:%M:%S)] update round done"
