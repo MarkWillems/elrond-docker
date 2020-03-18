a57893ba27b82e34b697f2f027412192199766ec
root <markwillems@gmail.com>
2020-03-18 08:08:05 +0000

    fixes to work with the updated scripts

--- elrond-node/install_elrond.sh
+++ elrond-node/install_elrond.sh
@@ -7,13 +7,22 @@ WORKDIR="$CUSTOM_HOME/elrond-nodes/node-0"
 mkdir -p $CUSTOM_HOME/elrond-nodes;
 mkdir -p $CUSTOM_HOME/elrond-utils;
 mkdir -p $CUSTOM_HOME/VALIDATOR_KEYS
+GITHUBTOKEN=""
 
+if [ -z "$GITHUBTOKEN" ]; then
+                  BINARYVER="tags/$(curl --silent "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+                  CONFIGVER="tags/$(curl --silent "https://api.github.com/repos/ElrondNetwork/elrond-config/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+
+            else
+             BINARYVER="tags/$(curl --silent -H "Authorization: token $GITHUBTOKEN" "https://api.github.com/repos/ElrondNetwork/elrond-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+             CONFIGVER="tags/$(curl --silent -H "Authorization: token $GITHUBTOKEN" "https://api.github.com/repos/ElrondNetwork/elrond-config/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+    fi
 echo $NUMBEROFNODES > /home/elrond/.numberofnodes
-
 sed -i 's/ubuntu/elrond/g' /home/elrond/elrond-go-scripts-v2/config/variables.cfg
 sed -n '/sudo systemctl/!p' -i /home/elrond/elrond-go-scripts-v2/config/functions.cfg
 sed -n '/sudo systemctl/!p' -i /home/elrond/elrond-go-scripts-v2/script.sh
-
+paths
+go_lang
 git_clone
 build_keygen
 build_node
